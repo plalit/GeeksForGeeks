3fcffe2b2b6afa29ce4573a4e67fc0c60550c733
plalit <lalit06pathak@gmail.com>
2016-12-27 14:11:29 -0500

    check consecutive numbers

---
+++ check_array_elements_consecutive_ARRAY.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+
+	int min = arr[0], max = arr[0];
+
+	for(int i=1;i<n;i++)
+	{
+		if(arr[i] > max)
+			max = arr[i];
+		if(arr[i] < min)
+			min = arr[i];
+	}
+	bool flag = false;
+	if(max-min+1 == n)
+		flag = true;
+
+	int brr[n];
+	for(int i=0;i<n;i++)
+		brr[i] = -1;
+
+	for(int i=0;i<n;i++)
+	{
+		int t = max - arr[i];
+//		cout<<t<<endl;
+		if(t < n)
+			brr[t]++;
+	}
+
+	/*for(int i=0;i<n;i++)
+		cout<<brr[i]<<" ";
+	cout<<endl;
+*/
+	bool flag2 = true;
+	for(int i=0;i<n;i++)
+	{
+		if(brr[i] != 0)
+		{
+			flag2 = false;
+			break;
+		}
+	}
+
+	if(flag && flag2)
+		cout<<"Consecutive Numbers"<<endl;
+	else
+		cout<<"Not Consecutive Numbers"<<endl;
+}
