0f6a8b34f810d51dd24cb72f7f12f9c3de507a56
plalit <lalit06pathak@gmail.com>
2017-01-02 00:00:25 -0500

    changes to the same commit last time

--- 4 numbers_equal_sum_ARRAY.cpp
+++ 4 numbers_equal_sum_ARRAY.cpp
@@ -11,6 +11,11 @@ struct sn
 	int total;
 };
 
+int compare (const void *a, const void * b)
+{
+    return ( (*(sn *)a).total - (*(sn*)b).total );
+}
+
 int main()
 {
 	int n,sum;
@@ -20,13 +25,13 @@ int main()
 	for(int i=0;i<n;i++)
 		cin>>arr[i];
 
-	int t = (n*(n+1))/2;
+	int t = (n*(n-1))/2;
 	struct sn brr[t];
 
 	int k = 0;
 	for(int i=0;i<n;i++)
 	{
-		for(int j=0;j<n;j++)
+		for(int j=i+1;j<n;j++)
 		{
 			brr[k].first = i;
 			brr[k].second = j;
@@ -35,16 +40,21 @@ int main()
 		}
 	}
 
+	qsort (brr, t, sizeof(brr[0]), compare);
+
+
 	int i=0, j = t-1;
 	while(j > i)
 	{
 		if(brr[i].total + brr[j].total == sum)
+		{
 			printf ("%d, %d, %d, %d\n", arr[brr[i].first], arr[brr[i].second],
                                      arr[brr[j].first], arr[brr[j].second]);
+			break;
+		}
 		else if(brr[i].total + brr[j].total < sum)
 			i++;
 		else
 			j--;
 	}
-
 }
