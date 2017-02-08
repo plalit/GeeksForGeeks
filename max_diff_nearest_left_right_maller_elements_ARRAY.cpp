22ca7480789dbb4d3b507a23ca7125c552976fa3
plalit <lalit06pathak@gmail.com>
2017-02-08 23:57:59 +0530

    .

--- max_diff_nearest_left_right_maller_elements_ARRAY.cpp
+++ max_diff_nearest_left_right_maller_elements_ARRAY.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
+#include<stack>
 
 using namespace std;
 
@@ -9,5 +10,29 @@ int main()
 	int n;
 	cin>>n;
 
-	
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+
+	int left[n],right[n];
+	for(int i=0;i<n;i++)
+	{
+		left[i] = -1;
+		right[i] = -1;
+	}
+
+	stack<int>s;
+
+	for(int i=0;i<n;i++)
+	{
+		while(!s.empty() && s.top > arr[i])
+			s.pop();
+
+		if(!s.empty)
+			left[i] = s.top;
+		else
+			left[i] = 0;
+
+		s.push(arr[i]);
+	}
+
 }
