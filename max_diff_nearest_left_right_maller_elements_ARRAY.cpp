841edb5163d552824753bbbbfe39faacb314be04
plalit <lalit06pathak@gmail.com>
2017-02-12 23:28:39 +0530

    easy one

--- max_diff_nearest_left_right_maller_elements_ARRAY.cpp
+++ max_diff_nearest_left_right_maller_elements_ARRAY.cpp
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stack>
+//#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -10,6 +11,7 @@ int main()
 	int n;
 	cin>>n;
 
+	int arr[n];
 	for(int i=0;i<n;i++)
 		cin>>arr[i];
 
@@ -20,19 +22,37 @@ int main()
 		right[i] = -1;
 	}
 
-	stack<int>s;
+	std::stack<int>s;
+	std::stack<int>r;
 
 	for(int i=0;i<n;i++)
 	{
-		while(!s.empty() && s.top > arr[i])
+		while(!s.empty() && s.top() >= arr[i])
 			s.pop();
 
-		if(!s.empty)
-			left[i] = s.top;
+		if(!s.empty())
+			left[i] = s.top();
 		else
 			left[i] = 0;
 
 		s.push(arr[i]);
 	}
+
+	for(int i=n-1;i>=0;i--)
+	{
+		while(!r.empty() && r.top() >= arr[i])
+			r.pop();
+
+		if(!r.empty())
+			right[i] = r.top();
+		else
+			right[i] = 0;
+
+		r.push(arr[i]);
+	} // 2, 4, 8, 7, 7, 9, 3
+
+	for(int i=0;i<n;i++)
+		cout<<right[i]<<" ";
+	cout<<endl;
 
 }
